efb8c4af2038b56ca43751fdce768c1168d550ee
elizatucker <elizaaqtucker@gmail.com>
2025-02-17 16:13:02 -0800

    new menu

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-#container {
-    display: flex;
+const button = document.querySelector("#button")
+const disclaimer = document.querySelector("#disclaimer")
+let showing = false
+
+const onClick = function() {
+   if (showing == false) {
+    disclaimer.innerHTML = "Oat milk, almond milk adds $0.60. Ingredient subsitutions and additions adds $1.00"
+    showing = true
+   } else {
+    disclaimer.innerHTML = ""
+    showing = false
+   }
 }
-#main {
-    color: green;
-    background-color: Blue;
-}
+
+button.addEventListener("click", onClick)
